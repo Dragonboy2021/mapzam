67de0aad868ec699c0ab392ba41c194aa503ed8b
Dragonboy2021 <ryustreetfighter2021@gmail.com>
2021-05-19 16:20:53 +0900

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  googleMapApi: process.env.GOOGLE_MAP_KEY,
+  pexelApi: process.env.PEXEL_KEY,
+  apiBase: 'http://localhost:3000'
 };
