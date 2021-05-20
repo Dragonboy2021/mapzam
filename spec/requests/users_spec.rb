8b43667e8bb03ad609be4cb51b41b5baaba219ac
Dragonboy2021 <ryustreetfighter2021@gmail.com>
2021-05-20 21:21:52 +0900

    updated score and user request spec files

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -1,40 +1,36 @@
 require 'rails_helper'
-# include ActionController::RespondWith
 
 RSpec.describe "Users Authentication", type: :request do
   before do
     @user = FactoryBot.create(:user)
   end
-  
-  
+
   describe 'GET /api/users/:id' do
     context 'Userデータが取得できるとき' do
-    
-    
+      it 'Headerを含めば登録済みのユーザーのデータを取得できる' do
+        auth = sign_in(@user)
+        get api_auth_validate_token_path, { headers: {
+          uid: auth["uid"],
+          client: auth["client"],
+          "access-token": auth["access-token"]
+        }}
+        user_data = JSON.parse response.body
+        id = user_data['data']['id']
+        get api_user_path(id), { headers: {
+          uid: auth["uid"],
+          client: auth["client"],
+          "access-token": auth["access-token"]
+        }}
+        expect(response).to have_http_status(200)
+      end
     end
     
-    
-    # it 'User情報が正しくレスポンスが返ってくる' do
-    #   auth = sign_in(@user)
-    #   post(api_scores_path, params: { score: 1}, headers: {
-    #     uid: auth["uid"],
-    #     client: auth["client"],
-    #     "access-token": auth["access-token"]
-    #   })
-    #   get api_auth_validate_token_path, { headers: {
-    #     uid: auth["uid"],
-    #     client: auth["client"],
-    #     "access-token": auth["access-token"]
-    #   }}
-    #   user_data = JSON.parse response.body
-    #   id = user_data['data']['id']
-    #   get api_user_path(id), { headers: {
-    #     uid: auth["uid"],
-    #     client: auth["client"],
-    #     "access-token": auth["access-token"]
-    #   }}
-      
-    #   expect(response).to have_http_status(200)
+    context 'Userデータが取得でないとき' do
+      it 'Headerがないと登録済みのユーザーのデータを取得できない' do
+        id = 1
+        get api_user_path(id)
+        expect(response).to have_http_status(401)
+      end
     end
   end
 end
