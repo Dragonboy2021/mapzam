5322c22d8407639bb0a6c9950a2dcf7bd0d919e7
Dragonboy2021 <ryustreetfighter2021@gmail.com>
2021-05-30 15:32:39 +0900

    Updated envrionment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   googleMapApi: process.env.GOOGLE_MAP_KEY,
   pexelApi: process.env.PEXEL_KEY,
-  apiBase: 'http://54.65.61.196:3000'
+  apiBase: 'http://54.65.61.196'
 };
