00752e89f67e346795dac73add271a9247479c5f
Dragonboy2021 <ryustreetfighter2021@gmail.com>
2021-05-31 11:55:36 +0900

    Revert "updated gitignore, removed webpack config, env.example, environment files and added environment example file"
    This reverts commit de7cfd811363394e6c2ac1f9c75fc70f9a006fbf.

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+  production: true,
+  googleMapApi: process.env.GOOGLE_MAP_KEY,
+  pexelApi: process.env.PEXEL_KEY,
+  apiBase: 'http://54.65.61.196'
+};
--- src/environments/environments.example.ts
+++
@@ -1,6 +0,0 @@
-export const environment = {
-  production: false,
-  googleMapApi: '',
-  pexelApi: '',
-  apiBase: 'http://localhost:3000'
-};
